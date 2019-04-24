a1890bc3e1ed7fd8a05df64316418c933ae49e59
TristanMoe <Trilleo@hotmail.com>
2019-04-24 11:15:56 +0200

    Added new integration plan, hopefully final

---
+++ Microwave.Test.Unit/Integrationstest/IT1/Door_UserInterface.cs
@@ -0,0 +1,25 @@
+﻿using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+
+namespace Microwave.Test.Unit.Integrationstest.IT1
+{
+    [TestFixture]
+    public class Door_UserInterface
+    {
+        private IButton _startCancelButton;
+        private IButton _powerButton;
+        private IButton _timerButton;
+        private IDisplay _display;
+        private ILight _light; 
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            _startCancelButton = 
+        }
+
+
+
+    }
+}
