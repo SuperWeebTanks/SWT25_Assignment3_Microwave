75e84a5b923a51a959fc72cfc5ec898e20ab4f01
TristanMoe <Trilleo@hotmail.com>
2019-04-25 15:34:03 +0200

    Added tets CookController_UserInterface

--- Microwave.Test.Unit/Integrationstest/IT2/CookController_UserInterface.cs
+++ Microwave.Test.Unit/Integrationstest/IT2/CookController_UserInterface.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Castle.DynamicProxy.Contributors;
 using MicrowaveOvenClasses.Boundary;
@@ -20,13 +21,14 @@ namespace Microwave.Test.Unit.Integrationstest.IT3
         private ILight _light;
         private ITimer _timer;
         private IPowerTube _tube;
-        private IUserInterface _ui;
-
+        private ICookController _stubCoooker; 
+        
         //Included Modules 
         private Button _startCancelButton;
         private Button _powerButton;
         private Button _timerButton;
         private Door _door;
+        private UserInterface _uiToIntegrate;
 
 
         //Driven Module 
@@ -35,14 +37,49 @@ namespace Microwave.Test.Unit.Integrationstest.IT3
         [SetUp]
         public void SetUp()
         {
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+            _timer = Substitute.For<ITimer>();
+            _tube = Substitute.For<IPowerTube>();
+            _stubCoooker = Substitute.For<ICookController>();
+
             _startCancelButton = new Button();
             _powerButton = new Button();
             _timerButton = new Button();
             _door = new Door();
-            _cookControllerDriven = new CookController(_timer, _display, _tube, _ui);
+            _uiToIntegrate = new UserInterface(_powerButton, _timerButton, _startCancelButton, _door, _display, _light, _stubCoooker);
+            _cookControllerDriven = new CookController(_timer, _display, _tube, _uiToIntegrate);
         }
 
+        [Test]
+        public void CookController_TimerExpiresAndCookControllerInformsUI_DisplayCleared()
+        {
+            //Act 
+            _powerButton.Press();
+            _timerButton.Press();
+            _startCancelButton.Press();
 
+           _cookControllerDriven.StartCooking(50, 60);
+           _timer.Expired += Raise.Event();
+
+            //Assert
+            _display.Received().Clear();
+        }
+
+        [Test]
+        public void CookController_TimerExpiresAndCookControllerInformsUI_TurnOffLight()
+        {
+            //Act 
+            _powerButton.Press();
+            _timerButton.Press();
+            _startCancelButton.Press();
+
+            _cookControllerDriven.StartCooking(50, 60);
+            _timer.Expired += Raise.Event();
+
+            //Assert
+            _light.Received().TurnOff();
+        }
 
     }
 }
